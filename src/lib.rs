a4a4bdd27a9d6a8f7cb39d5719c64381e4eeac0d
Michael Hewson <michael@michaelhewson.ca>
2019-04-20 18:23:17 -0400

    some cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
 /*!
 provides a Vec-like data structure for dynamically sized types
 */
-#![feature(alloc, unsize, conservative_impl_trait)]
-use std::marker::Unsize;
-use std::{ptr, mem};
+#![feature(alloc, raw_vec_internals, unsize)]
+use std::{
+    marker::Unsize,
+    ptr,
+    mem,
+};
 
 extern crate alloc;
 use alloc::raw_vec::RawVec;
 
 extern crate referent;
 use referent::Referent;
-
-mod iter;
-pub use iter::IntoIter;
 
 pub struct DSTVec<T: Referent + ?Sized> {
     // these aren't actual pointers, they're offsets + meta, used to
@@ -72,6 +72,61 @@ impl<T: Referent + ?Sized> Drop for DSTVec<T> {
             unsafe {
                 ptr::drop_in_place(self.assemble(offset, meta));
             }
+        }
+    }
+}
+
+/// An owning iterator over the contents of a DSTVec<T>. Each call to `next` will return a *mut pointer to T, and it is up to the caller to ensure that its value is dropped.
+pub struct IntoIter<T: Referent + ?Sized> {
+    pointers: ::std::vec::IntoIter<(usize, T::Meta)>,
+    data: RawVec<u8>,
+}
+
+impl<T: Referent + ?Sized> IntoIter<T> {
+    unsafe fn assemble(&self, offset: usize, meta: T::Meta) -> *mut T {
+        T::assemble_mut(self.data.ptr().offset(offset as isize) as *mut T::Data, meta)
+    }
+}
+
+impl<T: Referent + ?Sized> Iterator for IntoIter<T> {
+    type Item = *mut T;
+
+    fn next(&mut self) -> Option<*mut T> {
+        self.pointers.next().map(|(offset, meta)| {
+            unsafe {
+                self.assemble(offset, meta)
+            }
+        })
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.pointers.size_hint()
+    }
+}
+
+impl<T: Referent + ?Sized> ExactSizeIterator for IntoIter<T> {}
+
+impl<T: Referent + ?Sized> Drop for IntoIter<T> {
+    fn drop(&mut self) {
+        let offsets_with_metas = mem::replace(&mut self.pointers, Vec::new().into_iter());
+        for (offset, meta) in offsets_with_metas {
+            unsafe {
+                ptr::drop_in_place(self.assemble(offset, meta))
+            }
+        }
+    }
+}
+
+impl<T: Referent + ?Sized> IntoIterator for DSTVec<T> {
+    type Item = *mut T;
+    type IntoIter = IntoIter<T>;
+
+    fn into_iter(mut self) -> IntoIter<T> {
+        let pointers = mem::replace(&mut self.pointers, Vec::new());
+        let data = mem::replace(&mut self.data, RawVec::new());
+        IntoIter {
+            pointers: pointers.into_iter(),
+            data: data,
         }
     }
 }
